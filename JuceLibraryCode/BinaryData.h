4efdb1121e21d61233374e76b1e81ee211f58df3
mars <martin@rebel-it.com>
2012-01-02 17:33:08 +0000

    removed unwanted binary

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -10,9 +10,6 @@ namespace BinaryData
     extern const char*   blipbox_icns;
     const int            blipbox_icnsSize = 38378;
 
-    extern const char*   interrupt_o;
-    const int            interrupt_oSize = 688;
-
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
     const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
