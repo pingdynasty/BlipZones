a4ec5e6b528c55e8e9dd7c831e1deec9954458bc
mars <martin@rebel-it.com>
2012-02-03 10:52:58 +0000

    updated to use Juce datagrams

--- Source/OscServer.cpp
+++ Source/OscServer.cpp
@@ -2,14 +2,13 @@
 #include "OscServer.h"
 #include "OscAction.h"
 #include "../Libraries/osc/OscOutboundPacketStream.h"
-#include "../Libraries/osc/OscOutboundPacketStream.h"
 
 void OscServer::send(OscAction* action){
   char buffer[bufsize];
   osc::OutboundPacketStream stream(buffer, bufsize);
   stream << osc::BeginBundleImmediate
 	 << osc::BeginMessage(action->prefix.toUTF8())
-	 << action->value << action->suffix.toUTF8()
+	 << action->value
 	 << osc::EndMessage << osc::EndBundle;
 
   DatagramSocket socket(0);
